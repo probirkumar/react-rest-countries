d9dfc18ca673d90435cb5d4b2d141594f8e88922
ProbirKumare <probirsharma655@gmail.comm>
2024-03-31 12:08:20 +0600

    bootstrap connection

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {name, population, area, region, flags} = props.country;
+    const { name, population, area, region, flags } = props.country;
     console.log(props.country)
     return (
-        <div className='country'>
+        <div className='country bg-warning'>
             <h2>Country Name: {name.common}</h2>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
